fd9ffe491db705c5d5fc1ae22ccae0ca63af24f5
Sasha <alexander.goryachev@gmail.com>
2021-01-18 19:20:41 +0200

    learn props

---
+++ src/componens/search-panel/search-panel.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './search-panel.css';
+
+const SearchPanel = () => {
+
+  const searchText = 'Type here to search';
+  const searchStyle = {
+    fontSize: '20px'
+  };
+
+  return <input className={'search-input'}
+    placeholder={searchText}
+    style={searchStyle}/>;
+};
+
+export default SearchPanel;
